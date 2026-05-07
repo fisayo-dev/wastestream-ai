4471bca7f36ddabe4389b6701dc860a43fb12fd9
fisayo-dev <olufisayobadina@gmail.com>
2026-05-07 13:23:20 +0100

    chore: clean code

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -1,19 +1,16 @@
-import * as dotenv from "dotenv";
-
+import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { sql } from "drizzle-orm";
 import { Pool } from "pg";
 
-dotenv.config();
+config();
 
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
+if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
 export const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
