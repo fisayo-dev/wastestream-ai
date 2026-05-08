22e8100969b58a1793ba25322790ee4990bf4750
fisayo-dev <olufisayobadina@gmail.com>
2026-05-08 20:02:57 +0100

    chore: write correct auth Base url

--- web/lib/auth.ts
+++ web/lib/auth.ts
@@ -1,10 +1,9 @@
 import { WasteTypeOption } from "@/types/waste";
 
 export const apiBaseUrl =
-  process.env.NEXT_BACKEND_PUBLIC_URL;
+  process.env.NEXT_BACKEND_PUBLIC_URL ?? "http://localhost:2300/v1";
 
-export const authBaseUrl =
-  process.env.NEXT_BACKEND_AUTH_PUBLIC_URL;
+export const authBaseUrl =`${apiBaseUrl}/api/auth`;
 
 export const googleAuthUrl = `${authBaseUrl}/sign-in/social`;
 export const sessionUrl = `${apiBaseUrl}/auth/session`;
